1302c845cdd3100c42b7b42cabac97589f50d253
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-28 02:16:47 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -74,15 +74,15 @@ puts "Playing #{song.name} by #{song.artist.name}" if song
 
 def call
     while true
-    expect($stdout).to receive(:puts).with("Welcome to your music library!")
-      expect($stdout).to receive(:puts).with("To list all of your songs, enter 'list songs'.")
-      expect($stdout).to receive(:puts).with("To list all of the artists in your library, enter 'list artists'.")
-      expect($stdout).to receive(:puts).with("To list all of the genres in your library, enter 'list genres'.")
-      expect($stdout).to receive(:puts).with("To list all of the songs by a particular artist, enter 'list artist'.")
-      expect($stdout).to receive(:puts).with("To list all of the songs of a particular genre, enter 'list genre'.")
-      expect($stdout).to receive(:puts).with("To play a song, enter 'play song'.")
-      expect($stdout).to receive(:puts).with("To quit, type 'exit'.")
-      expect($stdout).to receive(:puts).with("What would you like to do?")
+    puts"Welcome to your music library!"
+      puts"To list all of your songs, enter 'list songs'."
+      puts"To list all of the artists in your library, enter 'list artists'."
+      puts"To list all of the genres in your library, enter 'list genres'."
+     puts"To list all of the songs by a particular artist, enter 'list artist'."
+    puts"To list all of the songs of a particular genre, enter 'list genre'."
+      puts"To play a song, enter 'play song'."
+     puts"To quit, type 'exit'."
+    puts"What would you like to do?"
       command = gets.strip
       case command
       when "exit"
